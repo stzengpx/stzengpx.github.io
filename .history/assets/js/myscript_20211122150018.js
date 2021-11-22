c9a7872714fbfd1a379778315228f34ea9b6d90b
stzengpx <st.zengpx@gmail.com>
2021-11-22 15:01:03 +0800

    feat(myscript.js): try redirect 202111221500

---
+++ .history/assets/js/myscript_20211122150018.js
@@ -0,0 +1,3 @@
+alert("hello world");
+alert("hello world");
+window.location.href="http://www.google.com";
---
+++ .history/assets/js/myscript_20211122150020.js
@@ -0,0 +1,3 @@
+alert("hello world");
+alert("hello world2");
+window.location.href="http://www.google.com";
